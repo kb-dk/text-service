825dec29b29833290ff310c8c21093710b32881f
bimo <zahramousavidk@gmail.com>
2018-10-03 13:45:52 +0200

    Put text possibilities in a separate toolbox

--- app/assets/javascripts/DkBreve.js
+++ app/assets/javascripts/DkBreve.js
@@ -197,44 +197,6 @@ $(document).on('kbosdready', function (e) {
 //////////////////////////////////////////////////////
 
 $(document).ready(function () {
-
-// set up handler for ocr fullscreen
-    $('#ocrFullscreenButton').click(function (e) {
-        // Copy/Pasted from http://stackoverflow.com/questions/7130397/how-do-i-make-a-div-full-screen /HAFE
-        // if already full screen; exit
-        // else go fullscreen
-        if (
-            document.fullscreenElement ||
-            document.webkitFullscreenElement ||
-            document.mozFullScreenElement ||
-            document.msFullscreenElement
-        ) {
-            if (document.exitFullscreen) {
-                document.exitFullscreen();
-            } else if (document.mozCancelFullScreen) {
-                document.mozCancelFullScreen();
-            } else if (document.webkitExitFullscreen) {
-                document.webkitExitFullscreen();
-            } else if (document.msExitFullscreen) {
-                document.msExitFullscreen();
-            }
-        } else {
-            var element = $('.ocr').get(0);
-            if (element.requestFullscreen) {
-                element.requestFullscreen();
-            } else if (element.mozRequestFullScreen) {
-                element.mozRequestFullScreen();
-            } else if (element.webkitRequestFullscreen) {
-                element.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-            } else if (element.msRequestFullscreen) {
-                element.msRequestFullscreen();
-            }
-        }
-    });
-
-// Close on fullscreen
-    $('.escFullScreenButton').click(dkBreve.closeFullScreen);
-
  // If there is no OSD in the page so there is no pagination
 // The following code checks if there is no pagination then 'on scroll' changes the page number on citation
     if(typeof (has_facs)!== 'undefined') {
--- app/assets/javascripts/text_service/toggle_text_possibilities.js
+++ app/assets/javascripts/text_service/toggle_text_possibilities.js
@@ -4,18 +4,20 @@
 
 function toggleExposable() {
     var text_possibility_elements = document.getElementsByClassName("exposableDocumentFunctions");
+    var text_possibility_toolbar = document.getElementsByClassName("textPossibilitiesToolbar")[0];
     var btn = document.getElementById("toggleTextPossibilities");
-
 
     if (btn.classList.contains('active')) {
         for (i = 0; i < text_possibility_elements.length; i++) {
             text_possibility_elements[i].style.display = 'none';
         }
+        text_possibility_toolbar.classList.add('hidden');
         btn.classList.remove("active");
     } else {
         for (i = 0; i < text_possibility_elements.length; i++) {
             text_possibility_elements[i].style.display = 'block';
+        }
+        text_possibility_toolbar.classList.remove('hidden');
+        btn.classList.add("active");
     }
-    btn.classList.add("active");
-}
 }
