58ae92809f82bff6d55e8b204c8c0eb2e0efc44a
bimo <zahramousavidk@gmail.com>
2018-10-08 16:41:19 +0200

    Removed comments

--- app/assets/javascripts/components/atoms/facet/facet.js
+++ app/assets/javascripts/components/atoms/facet/facet.js
@@ -4,13 +4,6 @@ const facetSections = document.querySelectorAll(
 
 Array.prototype.slice.call(facetSections).forEach((element) => {
   element.onclick = () => {
-    // Uncomment following if other open tabs should close when a new one opens
-    // Array.prototype.slice.call(facetSections).forEach((el) => {
-    //   if (el !== element) {
-    //     el.classList.remove('active')
-    //   }
-    // })
-    console.log(element);
     element.parentNode.classList.toggle('active')
   }
 })
