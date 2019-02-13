bd92ac5d099b82e8c156d3a0cba1c58812e48e30
zahra <zahramousavidk@gmail.com>
2019-02-13 02:04:06 -0800

    Fixed a bug in javascript

--- app/assets/javascripts/text_service/smaller_font_size.js
+++ app/assets/javascripts/text_service/smaller_font_size.js
@@ -1,10 +1,15 @@
 function resize_to_fit() {
-    var heading = document.getElementsByClassName("org-fc19c325-bb61-4c28-ad0e-7c272912ed78")[0].getElementsByTagName("h1")[0];
-    var text_size = heading.innerHTML.trim().length;
-    if (text_size >= 50) {
-        var fontsize = getComputedStyle(heading).fontSize;
-        newFontSize = (parseInt(fontsize, 10) - 15).toString() + "px";
-        document.getElementsByClassName("org-fc19c325-bb61-4c28-ad0e-7c272912ed78")[0].getElementsByTagName("h1")[0].style.fontSize = newFontSize;
+    var pageIntro = document.getElementsByClassName("org-fc19c325-bb61-4c28-ad0e-7c272912ed78")[0];
+    if (pageIntro) {
+        var heading = pageIntro.getElementsByTagName("h1")[0];
+        if (heading) {
+            var text_size = heading.innerHTML.trim().length;
+            if (text_size >= 50) {
+                var fontsize = getComputedStyle(heading).fontSize;
+                newFontSize = (parseInt(fontsize, 10) - 15).toString() + "px";
+                document.getElementsByClassName("org-fc19c325-bb61-4c28-ad0e-7c272912ed78")[0].getElementsByTagName("h1")[0].style.fontSize = newFontSize;
+            }
+        }
     }
 }
 
