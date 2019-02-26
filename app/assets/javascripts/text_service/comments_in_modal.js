ff4859b55b15a213ebd83d80dfcf16474436c4ef
zahra <zahramousavidk@gmail.com>
2019-02-26 05:21:57 -0800

    Fixed 'DTTP-123_Facebook og Twitter ikonerne i Footeren har ikke de korrekte URL'er'

--- app/assets/javascripts/text_service/comments_in_modal.js
+++ app/assets/javascripts/text_service/comments_in_modal.js
@@ -1,13 +1,15 @@
 var comments = document.querySelectorAll('a[title="Kommentar"]');
-comments.forEach(function(comment) {
-    comment.dataset.toggle = "modal";
-    comment.dataset.target = "#comment";
+if (comments.length ) {
+    comments.forEach(function (comment) {
+        comment.dataset.toggle = "modal";
+        comment.dataset.target = "#comment";
 
-    comment.addEventListener("click", function(event){
-        event.preventDefault()
-        modal = document.getElementById('comment');
-        modal_body = modal.getElementsByClassName('modal-body')[0];
-        modal_body.innerHTML = this.href;
+        comment.addEventListener("click", function (event) {
+            event.preventDefault()
+            modal = document.getElementById('comment');
+            modal_body = modal.getElementsByClassName('modal-body')[0];
+            modal_body.innerHTML = this.href;
+        });
+
     });
-
-});
+}
