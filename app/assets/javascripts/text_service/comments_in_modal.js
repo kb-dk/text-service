05d34b11091dc28e0667fac6c0e179a5e50d3ae3
zahra <zahramousavidk@gmail.com>
2019-02-28 00:53:05 -0800

    Added comment controller to be used in modal

--- app/assets/javascripts/text_service/comments_in_modal.js
+++ app/assets/javascripts/text_service/comments_in_modal.js
@@ -1,15 +1,24 @@
 var comments = document.querySelectorAll('a[title="Kommentar"]');
-if (comments.length ) {
-    comments.forEach(function (comment) {
-        comment.dataset.toggle = "modal";
-        comment.dataset.target = "#comment";
 
-        comment.addEventListener("click", function (event) {
-            event.preventDefault()
-            modal = document.getElementById('comment');
-            modal_body = modal.getElementsByClassName('modal-body')[0];
-            modal_body.innerHTML = this.href;
-        });
+for (i = 0; i < comments.length; i++) {
+    var comment = comments[i];
+    comment.dataset.toggle = "modal";
+    comment.dataset.target = "#comment";
 
+    comment.addEventListener("click", function (event) {
+        event.preventDefault()
+        modal = document.getElementById('comment');
+        modal_body = modal.getElementsByClassName('modal-body')[0];
+
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4 && this.status == 200) {
+                modal_body.innerHTML = this.responseText;
+            }
+        };
+        var url = this.href.substr(this.href.lastIndexOf('/') + 1).replace("root#", "shoot-");
+        url = '/comment/' + url
+        xhttp.open("GET", url, true);
+        xhttp.send();
     });
 }
