d266ecdc7d7a32f71328b480e1f9b306e49b2a9f
zahra <zahramousavidk@gmail.com>
2019-03-12 08:38:14 -0700

    Open links in new tab so we don't need to find our way back

--- app/assets/javascripts/text_service/qutation_service.js
+++ app/assets/javascripts/text_service/qutation_service.js
@@ -4,7 +4,7 @@ function toggle_vis_kun_denne_del() {
     let text_possibility_elements = document.getElementsByClassName("exposableDocumentFunctions");
     if (this.checked) {
         for (i = 0; i < text_possibility_elements.length; i++) {
-            text_possibility_elements[i].style.display = 'inline';
+            text_possibility_elements[i].style.display = 'block';
         }
     } else {
         for (i = 0; i < text_possibility_elements.length; i++) {
@@ -16,4 +16,10 @@ function toggle_vis_kun_denne_del() {
 let show_quotation_service = document.getElementById('Show_only_this_part');
 if (show_quotation_service) {
     show_quotation_service.addEventListener('click', toggle_vis_kun_denne_del, false);
+}
+
+// Open links in new tabs
+var links = document.querySelectorAll('.exposableDocumentFunctions a');
+for (var i = 0; i < links.length; i++) {
+    links[i].setAttribute('target', '_blank');
 }
