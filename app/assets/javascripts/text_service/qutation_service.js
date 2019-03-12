75c33a438e16cc097f5eca5d19c87a13f47eacd0
zahra <zahramousavidk@gmail.com>
2019-03-12 05:06:13 -0700

    Added a checkbox to textmuligheder so that 'vis kun denne del' can be checked on and off

---
+++ app/assets/javascripts/text_service/qutation_service.js
@@ -0,0 +1,17 @@
+"use strict";
+
+function toggle_vis_kun_denne_del() {
+    let text_possibility_elements = document.getElementsByClassName("exposableDocumentFunctions");
+    if (this.checked) {
+        for (i = 0; i < text_possibility_elements.length; i++) {
+            text_possibility_elements[i].style.display = 'inline';
+        }
+    } else {
+        for (i = 0; i < text_possibility_elements.length; i++) {
+            text_possibility_elements[i].style.display = 'none';
+        }
+    }
+}
+
+let show_quotation_service = document.getElementById('Show_only_this_part');
+show_quotation_service.addEventListener('click', toggle_vis_kun_denne_del, false);
