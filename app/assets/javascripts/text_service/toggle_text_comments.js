ddda0c3ee2bf1e6b7eb17f4f3c9aaee117ad4c13
zahra <zahramousavidk@gmail.com>
2019-05-13 06:17:39 -0700

    Added a 'toggleComments' option

---
+++ app/assets/javascripts/text_service/toggle_text_comments.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function toggle_text_comments() {
+    let text_comments = document.querySelectorAll("span.symbol.comment");
+    if (this.checked) {
+        for (i = 0; i < text_comments.length; i++) {
+            text_comments[i].style.display = 'inline';
+            text_comments[i].parentElement.style.textDecoration = 'underline';
+        }
+    } else {
+        for (i = 0; i < text_comments.length; i++) {
+            text_comments[i].style.display = 'none';
+            text_comments[i].parentElement.style.textDecoration = 'none';
+        }
+    }
+}
+
+let show_comments = document.getElementById('Show_comments');
+if (show_comments) {
+    show_comments.addEventListener('click', toggle_text_comments, false);
+}
