9d480ceff6eebf4b961a231da18b43fad32146d7
zahra <zahramousavidk@gmail.com>
2021-07-16 01:11:14 -0700

    Fixed styling for facet-range

---
+++ app/assets/javascripts/text_service/fix-date-range-visibility.js
@@ -0,0 +1,4 @@
+let date_range_container = document.querySelectorAll('.limit_content.range_limit')[0];
+if (date_range_container) {
+    date_range_container.parentElement.classList.add("range-limit-container");
+}
