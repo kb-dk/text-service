a610b85559e30c4155a0bb4ed668dd8ce894d0f8
zahra <zahramousavidk@gmail.com>
2021-07-27 01:35:42 -0700

    Fixed the disappearing title

--- app/assets/javascripts/text_service/toggle_titles.js
+++ app/assets/javascripts/text_service/toggle_titles.js
@@ -5,13 +5,13 @@ function toggle_title() {
     if (this.classList.contains('active')) {
         for (i = 0; i < text_titles.length; i++) {
             text_titles[i].style.display = 'none';
-            text_titles[i].parentElement.style.display = 'none';
+            text_titles[i].parentElement.classList.remove ('show_icons');
         }
     } else {
         for (i = 0; i < text_titles.length; i++) {
             text_titles[i].style.display = 'inline';
             text_titles[i].classList.add('icon', 'title');
-            text_titles[i].parentElement.style.display = 'inline';
+            text_titles[i].parentElement.classList.add ('show_icons');
         }
     }
     this.classList.toggle('active');
