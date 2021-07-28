f19bfd5e4be59208d94c6c957661f1a9b0bd3ab2
zahra <zahramousavidk@gmail.com>
2021-07-28 03:14:06 -0700

    Changed the href of the button when click on a recursive comment

--- app/assets/javascripts/text_service/comments_in_modal.js
+++ app/assets/javascripts/text_service/comments_in_modal.js
@@ -23,22 +23,34 @@ register_comments(comments);
 function register_comments(comments, embedded = false) {
     for (var i = 0; i < comments.length; i++) {
         var comment = comments[i];
-        if (!embedded) {
-          comment.dataset.toggle = "modal";
-        } else {
+        if (!embedded) { // Child modal
+            comment.dataset.toggle = "modal";
+        } else { // Parent modal
           comment.dataset.toggle = "";
         }
         comment.dataset.target = "#comment_modal";
 
-        comment.addEventListener("click", function (event) {
+        comment.addEventListener("click", onCommentClick);
+
+        function onCommentClick (event) {
             event.preventDefault()
             var modal = document.getElementById('comment_modal');
             var modal_body = modal.getElementsByClassName('modal-body')[0];
-            modal_body.innerHTML = '';
+            // modal_body.innerHTML = '';
             var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function () {
                 if (this.readyState === 4) {
                     if (this.status === 200) {
+                        // modal_body.innerHTML = (modal_body.innerHTML.toString().trim() !== '')?
+                        if(modal_body.innerHTML.toString().trim() !== ''){
+                            console.log(url);
+                            let backButton = document.getElementById('modal-back-button');
+                            backButton.style.visibility = 'visible';
+                            // backButton.href = url;
+                            // backButton.addEventListener("click", function (event) {
+
+                            // });
+                        }
                         modal_body.innerHTML = this.responseText;
                     } else {
                         console.error("Error:", this.statusText);
@@ -54,11 +66,15 @@ function register_comments(comments, embedded = false) {
                 }
             };
             var url = this.href.substr(this.href.lastIndexOf('/') + 1).replace("root#", "shoot-");
-            var document_url = this.href;
             url = '/comment/' + url
+            let backButton = document.querySelector('#modal-back-button');
+            if(backButton !== null){
+                backButton.href = backButton.dataset.currenturl;
+                backButton.dataset.currenturl = url;
+            }
             xhttp.open("GET", url, true);
             xhttp.send();
-        });
+        }
     }
 }
 
@@ -67,5 +83,4 @@ $('#comment_modal').on('hidden.bs.modal', function (e) {
     var modal = document.getElementById('comment_modal');
     var modal_body = modal.getElementsByClassName('modal-body')[0];
     modal_body.innerHTML = '';
-    console.log('hej hej');
 })
