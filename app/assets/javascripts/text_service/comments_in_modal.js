9d28b8825c4e4c35631d99b2651f3e2cf51dcf68
zahra <zahramousavidk@gmail.com>
2021-07-28 05:14:37 -0700

    Added back button url to the recursive modal

--- app/assets/javascripts/text_service/comments_in_modal.js
+++ app/assets/javascripts/text_service/comments_in_modal.js
@@ -1,9 +1,5 @@
 "use strict";
-var comments = document.querySelectorAll(
-    'a[title="Punktkommentar"], a[title="Kommentar"], a[title="Person"], a[title*="Sted"], a[title="Mytologi"], a[title="Titel"]'
-);
 var local_comments = document.querySelectorAll('a.Bibel');
-
 for (var i = 0; i < local_comments.length; i++) {
     var local_comment = local_comments[i];
     local_comment.dataset.toggle = "modal";
@@ -18,39 +14,25 @@ for (var i = 0; i < local_comments.length; i++) {
     });
 }
 
+var comments = document.querySelectorAll(
+    'a[title="Punktkommentar"], a[title="Kommentar"], a[title="Person"], a[title*="Sted"], a[title="Mytologi"], a[title="Titel"]'
+);
 register_comments(comments);
-
 function register_comments(comments, embedded = false) {
     for (var i = 0; i < comments.length; i++) {
         var comment = comments[i];
-        if (!embedded) { // Child modal
-            comment.dataset.toggle = "modal";
-        } else { // Parent modal
-          comment.dataset.toggle = "";
-        }
+        comment.dataset.toggle = "";
         comment.dataset.target = "#comment_modal";
-
         comment.addEventListener("click", onCommentClick);
-
         function onCommentClick (event) {
             event.preventDefault()
+            $('#comment_modal').modal('show');
             var modal = document.getElementById('comment_modal');
             var modal_body = modal.getElementsByClassName('modal-body')[0];
-            // modal_body.innerHTML = '';
             var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function () {
                 if (this.readyState === 4) {
                     if (this.status === 200) {
-                        // modal_body.innerHTML = (modal_body.innerHTML.toString().trim() !== '')?
-                        if(modal_body.innerHTML.toString().trim() !== ''){
-                            console.log(url);
-                            let backButton = document.getElementById('modal-back-button');
-                            backButton.style.visibility = 'visible';
-                            // backButton.href = url;
-                            // backButton.addEventListener("click", function (event) {
-
-                            // });
-                        }
                         modal_body.innerHTML = this.responseText;
                     } else {
                         console.error("Error:", this.statusText);
@@ -67,7 +49,7 @@ function register_comments(comments, embedded = false) {
             };
             var url = this.href.substr(this.href.lastIndexOf('/') + 1).replace("root#", "shoot-");
             url = '/comment/' + url
-            let backButton = document.querySelector('#modal-back-button');
+            let backButton = document.getElementsByClassName('modal-back-button')[0];
             if(backButton !== null){
                 backButton.href = backButton.dataset.currenturl;
                 backButton.dataset.currenturl = url;
